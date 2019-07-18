5a93fc6d368e565186a34a0a5fe10ef7904e1265
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-18 09:08:07 -0300

    Utilizando Where, Between, filtrando por mês

---
+++ Querys/DogaoFiltrandoMes.sql
@@ -0,0 +1,24 @@
+﻿select 
+		Tipo,
+		SUM(Vendas) as 'Total Vendas',
+		MONTH(DataVenda) as 'Mês Venda'
+from FoodTruckZezinho
+where MONTH(DataVenda) BETWEEN 4 AND 6 -- Traz somente as informações ENTRE os meses selecionados
+group by Tipo, MONTH(DataVenda)
+order by 'Total Vendas' desc
+
+
+
+--select 'Total' as 'Dogao',
+--			SUM(Temp.Total) as 'Vendas',
+--			Temp.MesVenda
+--		from (select
+--					Tipo,
+--					Vendas,
+--					(SUM(Vendas)) as 'Total',
+--					MONTH(DataVenda) as 'MesVenda'
+--				from FoodTruckZezinho	
+--			group by Tipo, Vendas, MONTH(DataVenda)) Temp
+--	group by Temp.MesVenda
+
+
