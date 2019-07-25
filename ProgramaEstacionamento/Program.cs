1d6b17fd9ae400445ad9b6c9de1e660091095b80
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-25 17:24:55 -0300

    Exercicio Estacionamento com Class, ainda sem Menu.

---
+++ ProgramaEstacionamento/Program.cs
@@ -0,0 +1,42 @@
+﻿using ProgramaEstacionamento.ControleEstacionamento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramaEstacionamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Um estacionamento deseja controlar a entrada de carros no estacionamento.
+            - Deve ser registrado o modelo do carro, a placa, a hora de entrada e a hora de saída.
+            - A placa deve ser a chave de pesquisa para o carro.
+            - Quando registrar a entrada do carro a hora de saída ficará nula;
+            - Quando o veículo sair do estacionamento, deve ser alterado a hora de saída;
+
+            - Listar todos os registros onde não ocorreu a saída;
+            - Listar todos os registros;
+            - Listar um registro pesquisando pela placa.
+
+            Criar a classe com os métodos de entrada, saída do estacionamento e listagem em um projeto 
+            e a aplicação para cadastrar os carros entrando no estacionamento em outro projeto. */
+
+            Estacionamento metodo = new Estacionamento();
+
+
+            metodo.EntradaCarro();
+
+            metodo.SaidaCarro();
+
+            metodo.ListarCarros();
+
+            metodo.ListarCarrosEstacionados();
+
+            Console.ReadKey();
+
+        }
+    }
+}
