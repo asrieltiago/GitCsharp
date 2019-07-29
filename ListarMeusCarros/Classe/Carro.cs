b70cf08cd4cc92a1739d4dbc6b448edbe526a4da
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-29 17:27:43 -0300

    Lista Tipada Carros com Switch Case / Menu

---
+++ ListarMeusCarros/Classe/Carro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListarMeusCarros.Classe
+{
+    public class Carro
+    {
+        public string Modelo { get; set; }
+        public int Ano { get; set; }
+        public string Placa { get; set; }
+        public int Cv { get; set; }
+
+    }
+}
