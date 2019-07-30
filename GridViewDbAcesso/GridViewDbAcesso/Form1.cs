0924b41b36773a868a1513bf46e4f7070100ab2d
Treinamento HBSIS <proway@hbsis.com.br>
2019-07-30 14:48:51 -0300

    DB com Forms

---
+++ GridViewDbAcesso/GridViewDbAcesso/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridViewDbAcesso
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'querysInnerJoinDataSet.Vendas' table. You can move, or remove it, as needed.
+            this.vendasTableAdapter.Fill(this.querysInnerJoinDataSet.Vendas);
+            // TODO: This line of code loads data into the 'querysInnerJoinDataSet.Usuarios' table. You can move, or remove it, as needed.
+            this.usuariosTableAdapter.Fill(this.querysInnerJoinDataSet.Usuarios);
+            // TODO: This line of code loads data into the 'querysInnerJoinDataSet.Marcas' table. You can move, or remove it, as needed.
+            this.marcasTableAdapter.Fill(this.querysInnerJoinDataSet.Marcas);
+            // TODO: This line of code loads data into the 'querysInnerJoinDataSet.Carros' table. You can move, or remove it, as needed.
+            this.carrosTableAdapter.Fill(this.querysInnerJoinDataSet.Carros);
+
+        }
+    }
+}
