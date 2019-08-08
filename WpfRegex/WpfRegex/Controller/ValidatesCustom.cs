89c4c2d74f1dc63b9933181fd2be5351e5aa9d27
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-08 15:19:55 -0300

    Commit Regex Email e Telefone

---
+++ WpfRegex/WpfRegex/Controller/ValidatesCustom.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WpfRegex.Controller
+{
+    public class ValidatesCustom
+    {
+        public static bool ValidEmail(string email)
+        {
+            var emailPattern = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
+                     + "@"
+                     + @"((([\w]+([-\w]*[\w]+)*\.)+[a-zA-Z]{2,4})|"
+                     + @"((([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]).){3}[01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))\z";
+
+            Regex regexmail = new Regex(emailPattern);
+            return regexmail.IsMatch(email);
+        }
+
+        public static bool ValidPhone(string telefone)
+        {
+            var phonePattern = @"^(?<countryCode>[\+][1-9]{1}[0-9]{0,2}\s)?(?<areaCode>0?[1-9]\d{0,4})(?<number>\s[1-9][\d]{5,12})(?<extension>\sx\d{0,4})?$";
+
+            Regex regexphone = new Regex(phonePattern);
+            return regexphone.IsMatch(telefone);
+        }
+    }
+}
---
+++ WpfRegex/WpfRegex/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfRegex.Controller;
+
+namespace WpfRegex
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSalvar_Click(object sender, RoutedEventArgs e)
+        {                        
+            string email = tbxEmail.Text;
+            string telefone = tbxTelefone.Text;
+
+            if (ValidatesCustom.ValidPhone(telefone))
+            {
+                MessageBox.Show($"{tbxNome.Text.ToUpper()}! O telefone {telefone} é VÁLIDO.", "Informação", MessageBoxButton.OK, MessageBoxImage.Information);
+                resTelefone.Text = "True";
+            }
+
+            else
+            {
+                MessageBox.Show($"ATENÇÃO {tbxNome.Text.ToUpper()}, o telefone {tbxTelefone.Text} é INVÁLIDO.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                resTelefone.Text = "False";
+            }
+
+            if (ValidatesCustom.ValidEmail(email))
+            {
+                MessageBox.Show($"{tbxNome.Text.ToUpper()}! O email {email} é VÁLIDO.", "Informação", MessageBoxButton.OK, MessageBoxImage.Information);
+                resEmail.Text = "True";
+            }
+
+            else
+            {
+                MessageBox.Show($"ATENÇÃO {tbxNome.Text.ToUpper()}, o email {tbxEmail.Text} é INVÁLIDO.", "Erro", MessageBoxButton.OK, MessageBoxImage.Error);
+                resEmail.Text = "False";
+            }
+        }
+
+    }
+}
