3394971f3e2a7e69e9cb853c2286f0e757477642
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-23 15:24:29 -0300

    Commit WEB Api Usuarios com Login, Senha e Validações

---
+++ RevisaoWEBApi/RevisaoWEBApi/Models/CustomValidFields.cs
@@ -0,0 +1,63 @@
+﻿using RevisaoWEBApi.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace RevisaoWEBApi.Models
+{
+    public class CustomValidFields : ValidationAttribute
+    {
+        ContextDB dB = new ContextDB();
+
+        private ValidFields typeField;
+
+        public CustomValidFields(ValidFields type)
+        {
+            typeField = type;
+        }
+
+        
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            //Se o valor não for nulo
+            if(value != null)
+            {
+                switch (typeField)
+                {
+                    case ValidFields.ValidaLogin:
+                        
+                        break;
+                    case ValidFields.ValidaEmail:
+                        return ValidarEmail(value, validationContext.DisplayName);   
+
+                    case ValidFields.ValidaSenha:
+                        break;
+                    case ValidFields.ValidaNome:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return new ValidationResult($"O campo {validationContext.DisplayName} é obrigatório.");
+        }
+
+        /// <summary>
+        /// Método utilizado para validar um email, utilizando-se de um pattern.
+        /// </summary>
+        /// <param name="value">Campo que verifica se o Email informado é válido</param>
+        /// <param name="displayField">Field que está sendo preenchido</param>
+        /// <returns></returns>
+        private ValidationResult ValidarEmail(object value, string displayField)
+        {
+            bool result = Regex.IsMatch(value.ToString(), @"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
+            if (result)
+                return ValidationResult.Success;
+
+            return new ValidationResult($"O campo {displayField} é inválido.");
+        }
+    }
+}
---
+++ RevisaoWEBApi/RevisaoWEBApi/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RevisaoWEBApi.Models
+{
+    public class Usuario : UserControls
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [CustomValidFields(Enums.ValidFields.ValidaNome)]
+        public string Nome { get; set; }
+
+        [CustomValidFields(Enums.ValidFields.ValidaEmail)]        
+        public string Email { get; set; }
+
+        [CustomValidFields(Enums.ValidFields.ValidaLogin)]
+        public string Login { get; set; }
+
+        [CustomValidFields(Enums.ValidFields.ValidaSenha)]
+        [MaxLength(16,ErrorMessage = "O campo excedeu o limite de caracteres \"16\".")]
+        [MinLength(8, ErrorMessage = "O campo não atende a quantidade minima de caracteres \"8\".")]
+        public string Senha { get; set; }
+
+    }
+}
